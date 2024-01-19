bdc4674157316d1c6294d5332b639b5668d869d0
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2024-01-19 14:05:41 +0700

    add stripe order

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import dbConnect from '../config/dbConnect.js';
 import userRoutes from '../routes/usersRoute.js';
@@ -19,6 +20,8 @@ dbConnect();
 
 const app = express();
 
+// cors
+app.use(cors());
 // logger
 app.use(logger);
 // pass incoming data
--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,6 +3,11 @@ import Order from "../models/Order.js";
 import User from "../models/User.js";
 import { createError } from "../helpers/createError.js";
 import Product from "../models/Product.js";
+import dotenv from 'dotenv';
+import Stripe from "stripe";
+
+dotenv.config();
+const stripe = new Stripe(process.env.STRIPE_KEY); 
 
 /**
  * @desc   Create orders
@@ -54,13 +59,31 @@ export const createOrderController = asyncHandler(async (req, res) => {
      await user.save();  
 
     // make payment (stripe)
+    const session = await stripe.checkout.sessions.create({
+        line_items: [{
+            price_data: {
+                currency: "usd",
+                product_data: {
+                    name: 'Hats',
+                    description: 'Best Hats Minimalist'
+                },
+                unit_amount: 10 * 100
+            },
+            quantity: 2,
+        }],
+        mode: 'payment',
+        success_url: 'http://localhost:3000/success',
+        cancel_url: 'http://localhost:3000/cancle'
+    })
+
+    res.send({ url: session.url })
 
     // payment webhook
     
-    res.status(201).json({
-        success: true,
-        message: "Order Successfully Created",
-        order,
-        user
-    })
+    // res.status(201).json({
+    //     success: true,
+    //     message: "Order Successfully Created",
+    //     order,
+    //     user
+    // })
 });
